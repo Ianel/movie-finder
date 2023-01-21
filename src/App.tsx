9c6f98442626552edf2cac37a3709f59bd21e5ff
Ianel Tombozafy <ianell.s118@gmail.com>
2023-01-21 10:36:09 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+    return <div className="bg-blue-500">hdfv</div>;
+}
+
+export default App;
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const HomePage = () => {
+    return (
+        <div className="min-w-screen min-h-screen bg-home">
+            <div className="w-full h-full bg-black opacity-50">
+                <input type="search" name="search" id="search" />
+            </div>
+        </div>
+    );
+};
+
+export default HomePage;
